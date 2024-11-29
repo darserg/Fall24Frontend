f8943f214f8ee19f7a985b6634dc38a0ce63e143
Darserg <Darserg1212>
2024-11-30 01:14:04 +0400

    Initial Commit

---
+++ Fall24Mac/Fall24MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fall24MacApp.swift
+//  Fall24Mac
+//
+//  Created by Сергей Дарьин on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Fall24MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
